90a609b593929b2853253cd72710d4685453d45d
GengBin <1371103410@qq.com>
2022-12-15 20:31:56 +0800

    删除无用代码

--- app/src/main/java/com/luck/pictureselector/SimpleActivity.java
+++ app/src/main/java/com/luck/pictureselector/SimpleActivity.java
@@ -14,21 +14,13 @@ public class SimpleActivity extends AppCompatActivity implements View.OnClickLis
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_other);
         Button btn_activity = findViewById(R.id.btn_activity);
-        Button btn_inject_fragment = findViewById(R.id.btn_inject_fragment);
-        Button btn_only_query_data = findViewById(R.id.btn_only_query_data);
         btn_activity.setOnClickListener(this);
-        btn_inject_fragment.setOnClickListener(this);
-        btn_only_query_data.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
         if (v.getId() == R.id.btn_activity) {
             startActivity(new Intent(SimpleActivity.this, MainActivity.class));
-        } else if (v.getId() == R.id.btn_inject_fragment){
-            startActivity(new Intent(SimpleActivity.this, InjectFragmentActivity.class));
-        } else if (v.getId() == R.id.btn_only_query_data){
-            startActivity(new Intent(SimpleActivity.this, OnlyQueryDataActivity.class));
         }
     }
 }
